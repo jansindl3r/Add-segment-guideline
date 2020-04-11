56c159536d1d49c50e870ace08384c6ae08aa3c7
Jan Sindler <jansindler@jansindl3r.local>
2020-04-11 14:10:19 +0200

    version 0.2

--- AddSegmentGuideline.roboFontExt/lib/addGuideline.py
+++ AddSegmentGuideline.roboFontExt/lib/addGuideline.py
@@ -1,39 +1,53 @@
 from math import atan2, degrees
-from lib.fontObjects.fontPartsWrappers import RGuideline, RGlyph
+from lib.fontObjects.fontPartsWrappers import RGuideline, RGlyph, RPoint
 from typing import Tuple, Union
 
 
-def angleBetweenTwoPoints(pt1: RGlyph, pt2: RGlyph) -> float:
+def angleBetweenTwoPoints(pt1: RPoint, pt2: RPoint) -> float:
     x: int
     y: int
     x, y = tuple(i - j for i, j in zip(pt1.position, pt2.position))
     return degrees(atan2(y, x))%180
 
 
-def middleBetweenTwoPoints(pt1: RGlyph, pt2: RGlyph) -> Tuple[int, int]:
+def middleBetweenTwoPoints(pt1: RPoint, pt2: RPoint) -> Tuple[int, int]:
     sums: Tuple[int, int] = tuple(map(sum, zip(pt1.position, pt2.position)))
     return tuple(map(lambda x:x//2, sums))
 
+def appendGuideline(ptFrom: RPoint, ptTo: RPoint, glyph: RGlyph) -> None:
+    angle: float = angleBetweenTwoPoints(ptFrom, ptTo)
+    position: Tuple[int, int] = middleBetweenTwoPoints(ptFrom, ptTo)
+    glyph.appendGuideline(
+        position, angle, color=(0.999, 0.001, 0, 0.499)
+    )
 
 def addGuideline(glyph: RGlyph) -> None:
-    for contour in glyph:
-        for i, segment in enumerate(contour):
-            if segment.selected:
-                if segment.type == "line":
-                    ptTo = segment.points[-1]
-                    ptFrom = contour[i - 1].points[-1]
-                    angle: float = angleBetweenTwoPoints(ptFrom, ptTo)
-                    position: Tuple[int, int] = middleBetweenTwoPoints(ptFrom, ptTo)
-                    glyph.appendGuideline(
-                        position, angle, color=(0.999, 0.001, 0, 0.499)
-                    )
+    print(len(glyph.selection))
+    if len(glyph.selection) == 1:
+        ptTo, *_ = glyph.selection
+        if ptTo.type == 'offcurve':
+            index = ptTo.index
+            contour = ptTo.contour.points
+            ptFrom = contour[index+1]
+            if ptFrom.type == 'offcurve':
+                ptFrom = contour[index-1]
+            appendGuideline(ptFrom, ptTo, glyph)
+    if len(glyph.selection) == 2:
+        appendGuideline(*glyph.selection, glyph)
+    else:
+        for contour in glyph:
+            for i, segment in enumerate(contour):
+                if segment.selected:
+                    if segment.type == "line":
+                            ptTo = segment.points[-1]
+                            ptFrom = contour[i - 1].points[-1]
+                            appendGuideline(ptFrom, ptTo, glyph)
+                    
     glyph.update()
 
 
 if __name__ == "__main__":
-    try:
-        glyph: RGlyph = CurrentGlyph()
-        with glyph.undo(f"Added segment guideline in {glyph.name}"):
-            addGuideline(glyph)
-    except:
-        pass
+    glyph: RGlyph = CurrentGlyph()
+    with glyph.undo(f"Added segment guideline in {glyph.name}"):
+        addGuideline(glyph)
+
--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # Add-segment-guideline
 - select a segment, and run the extension. 
-- By default it is on `cmd + /` shortcut, if you wish to remove them press `ctrl + /`. 
-- It will only remove the guideliens that had been added by this extension.
-- works only with straight lines and multiple selection <br>
+- By default it is on `cmd + /` shortcut, if you wish to remove them press `ctrl + /`. It will only remove the guideliens that had been added by this extension.
+- works only with straight lines only if multiple selection
+- can add guideline on offcurve point
+- can add guideline between two independent points, like two offcurve points for example
+## plans
+- add right click contextual menu for adding guideline - for mouse users
+- add possibility to mirror a guideline in contextual menu
+## verions
+- 0.2 - added possibility of adding guideline on offcurve points or two independent points (combination of on curve and offcurve possible). Fixed bug, all angles are `x:x % 180`
+- 0.1 - basic functionality, adding guideline on straight lines only
 ![Extension in use](guidelines.png "Result of this extension")
